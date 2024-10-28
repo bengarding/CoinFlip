61a5a6af6351b0d72592dfb989ef536fc92d514d
Ben Garding <70295296+bengarding@users.noreply.github.com>
2024-10-28 12:56:42 -0600

    :fire: new feature alert dialog

--- watch-app/src/main/java/com/helsinkiwizard/cointoss/ui/MainActivity.kt
+++ watch-app/src/main/java/com/helsinkiwizard/cointoss/ui/MainActivity.kt
@@ -8,46 +8,28 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.lifecycleScope
-import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.PositionIndicator
-import androidx.wear.compose.material.Scaffold
-import androidx.wear.compose.material.Text
-import androidx.wear.compose.material.dialog.Dialog
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
-import com.google.android.horologist.compose.layout.fillMaxRectangle
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.helsinkiwizard.cointoss.Constants.APP_DRAWER
 import com.helsinkiwizard.cointoss.Constants.EXTRA_START_FLIPPING
 import com.helsinkiwizard.cointoss.Constants.TILE
-import com.helsinkiwizard.cointoss.R
 import com.helsinkiwizard.cointoss.Repository
 import com.helsinkiwizard.cointoss.coin.Coin
 import com.helsinkiwizard.cointoss.coin.CoinList
 import com.helsinkiwizard.core.coin.CoinType
 import com.helsinkiwizard.core.theme.CoinTossTheme
-import com.helsinkiwizard.core.theme.Eight
-import com.helsinkiwizard.core.theme.Text20
-import com.helsinkiwizard.core.theme.Twelve
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
@@ -73,7 +55,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             CoinTossTheme {
                 CoinToss(initialCoinType)
-                CustomCoinDialog()
             }
         }
 
@@ -115,52 +96,6 @@ class MainActivity : ComponentActivity() {
                     )
 
                     1 -> CoinList()
-                }
-            }
-        }
-    }
-
-    @Composable
-    private fun CustomCoinDialog() {
-        val showCustomCoinDialog = repo.getShowSendToWatchDialog.collectAsState(initial = false).value
-        Dialog(
-            showDialog = showCustomCoinDialog,
-            onDismissRequest = {
-                // do nothing
-            }
-        ) {
-            val scrollState = rememberScrollState()
-            Scaffold(
-                positionIndicator = { PositionIndicator(scrollState) }
-            ) {
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(Twelve),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier
-                        .fillMaxRectangle()
-                        .verticalScroll(scrollState)
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.new_feature_alert),
-                        fontSize = Text20,
-                        textAlign = TextAlign.Center
-                    )
-                    Text(
-                        text = stringResource(id = R.string.custom_coin_dialog_message),
-                        textAlign = TextAlign.Center
-                    )
-                    Button(
-                        onClick = {
-                            lifecycleScope.launch {
-                                repo.disableShowSendToWatchDialog()
-                            }
-                        },
-                        modifier = Modifier.padding(top = Eight)
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.custom_coin_dialog_got_it)
-                        )
-                    }
                 }
             }
         }
