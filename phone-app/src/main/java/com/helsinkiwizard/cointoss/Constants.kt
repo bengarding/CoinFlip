33fc298c55896263baec11b0a8ec8b9bdae68dcd
bengarding <70295296+bengarding@users.noreply.github.com>
2024-12-01 22:56:24 -0700

    add ad banner

--- phone-app/src/main/java/com/helsinkiwizard/cointoss/Constants.kt
+++ phone-app/src/main/java/com/helsinkiwizard/cointoss/Constants.kt
@@ -7,4 +7,8 @@ internal object Constants {
 
     // Database
     const val DATABASE_NAME = "coin_toss_database"
+
+    // Ads
+    const val DEBUG_BANNER_AD_ID = "ca-app-pub-3940256099942544/6300978111"
+    const val BANNER_AD_ID = "ca-app-pub-6145401003007704/4529724834"
 }
--- phone-app/src/main/java/com/helsinkiwizard/cointoss/PhoneApplication.kt
+++ phone-app/src/main/java/com/helsinkiwizard/cointoss/PhoneApplication.kt
@@ -1,12 +1,16 @@
 package com.helsinkiwizard.cointoss
 
 import android.app.Application
+import com.google.android.gms.ads.MobileAds
 import com.helsinkiwizard.cointoss.Constants.REVENUE_CAT_KEY
 import com.helsinkiwizard.core.utils.logging.CrashlyticsTree
 import com.revenuecat.purchases.LogLevel
 import com.revenuecat.purchases.Purchases
 import com.revenuecat.purchases.PurchasesConfiguration
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 @HiltAndroidApp
@@ -17,6 +21,9 @@ class PhoneApplication : Application() {
         initTimber()
         Purchases.logLevel = LogLevel.DEBUG
         Purchases.configure(PurchasesConfiguration.Builder(this, REVENUE_CAT_KEY).build())
+        CoroutineScope(Dispatchers.IO).launch {
+            MobileAds.initialize(this@PhoneApplication)
+        }
     }
 
     private fun initTimber() = when {
