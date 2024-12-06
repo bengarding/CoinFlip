5ae4144a38cc2f79a9a8dd7d08ba0740a9e9c203
Ben Garding <70295296+bengarding@users.noreply.github.com>
2024-12-06 11:26:07 -0700

    remove AppLifecycleObserver

--- watch-app/src/main/java/com/helsinkiwizard/cointoss/WatchApplication.kt
+++ watch-app/src/main/java/com/helsinkiwizard/cointoss/WatchApplication.kt
@@ -1,7 +1,6 @@
 package com.helsinkiwizard.cointoss
 
 import android.app.Application
-import androidx.lifecycle.ProcessLifecycleOwner
 import com.google.android.gms.wearable.Wearable
 import com.helsinkiwizard.core.CoreConstants.WEAR_CAPABILITY
 import com.helsinkiwizard.core.utils.logging.CrashlyticsTree
@@ -11,12 +10,9 @@ import timber.log.Timber
 @HiltAndroidApp
 class WatchApplication : Application() {
 
-    private val lifecycleObserver = AppLifecycleObserver()
-
     override fun onCreate() {
         super.onCreate()
         Wearable.getCapabilityClient(this).addLocalCapability(WEAR_CAPABILITY)
-        ProcessLifecycleOwner.get().lifecycle.addObserver(lifecycleObserver)
         initTimber()
     }
 
